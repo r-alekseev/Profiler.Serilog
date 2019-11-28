df758a6cc81d8909065ff84a9a56827a453ccdf0
r-alekseev <r.alekseev@parcsis.org>
2019-11-29 00:27:09 +0400

    formatter for reports

--- src/Profiler.ReportWriter.Serilog/DefaultSerilogReportFormatter.cs
+++ src/Profiler.ReportWriter.Serilog/DefaultSerilogReportFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Profiler
 {
@@ -7,7 +8,29 @@ namespace Profiler
     {
         public IEnumerable<(string messageTemplate, object[] propertyValues)> Format(IEnumerable<ISectionMetrics> metricsCollection)
         {
-            throw new NotImplementedException();
+            var aggregatedMetrics = metricsCollection
+                .GroupBy(
+                    keySelector: m => m.Chain,
+                    elementSelector: m => m,
+                    resultSelector: (key, list) => new {
+                        Chain = key,
+                        Count = list.Sum(m => m.Count),
+                        ElapsedMilliseconds = list.Sum(m => m.Elapsed.TotalMilliseconds)
+                    });
+
+            string messageTemplate = "{chain} {count} {elapsedMs}";
+
+            foreach(var metrics in aggregatedMetrics)
+            {
+                object[] propertyValues = new object[]
+                {  
+                    metrics.Chain.ToString(),
+                    metrics.Count,
+                    metrics.ElapsedMilliseconds
+                };
+
+                yield return (messageTemplate, propertyValues);
+            }
         }
     }
 }
