6697e2aab4baaa2cbaf93e3d3ea3a856acb790f3
Roman Alekseev <r-alekseev@tuta.io>
2019-11-29 19:18:58 +0300

    simple default formatter for reports

--- src/Profiler.ReportWriter.Serilog/DefaultSerilogReportFormatter.cs
+++ src/Profiler.ReportWriter.Serilog/DefaultSerilogReportFormatter.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Profiler
 {
     internal class DefaultSerilogReportFormatter : ISerilogReportFormatter
     {
+        private readonly IEqualityComparer<string[]> _chainEqualityComparer = new ChainEqualityComparer();
+
+        private readonly string _messageTemplate = "{chain} {count} {elapsedMs}";
+
+        private readonly Func<string[], string> _formatChain = chain => string.Join(", ", chain);
+
         public IEnumerable<(string messageTemplate, object[] propertyValues)> Format(IEnumerable<ISectionMetrics> metricsCollection)
         {
-            throw new NotImplementedException();
+            var aggregatedMetrics = metricsCollection
+                .GroupBy(
+                    keySelector: m => m.Chain,
+                    elementSelector: m => m,
+                    resultSelector: (key, list) => new
+                    {
+                        Chain = key,
+                        Count = list.Sum(m => m.Count),
+                        Ticks = list.Sum(m => m.Elapsed.Ticks),
+                    },
+                    comparer: _chainEqualityComparer);
+
+            foreach (var metrics in aggregatedMetrics)
+            {
+                object[] propertyValues = new object[]
+                {
+                    _formatChain(metrics.Chain),
+                    metrics.Count,
+                    TimeSpan.FromTicks(metrics.Ticks).TotalMilliseconds
+                };
+
+                yield return (_messageTemplate, propertyValues);
+            }
         }
     }
 }
